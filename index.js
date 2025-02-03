ed98b9be381db8177a57ad139bf172f729e651ec
Lee <leeganghee4954@naver.com>
2025-02-03 17:26:14 +0900

    chore

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require("path");
+const express = require("express");
+const app = express();
 const bodyparser = require("body-parser");
-const fs=require("fs");
-const http=require('http');
+const fs = require("fs");
+const http = require("http");
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/', express.static("./"))
-app.listen(3000,function(err){
-    if(err) throw err;
-    console.log("start3000");
+app.use("/", express.static("./"));
+app.listen(3000, function (err) {
+  if (err) throw err;
+  console.log("start3000");
 });
 
-app.get('/', function(req, res){
-    res.render('index', {title : 'main'})
-})
+app.get("/", function (req, res) {
+  res.render("index", { title: "main" });
+});
 
+// hi
