6105ba6be7d46ba449b529da3ff849fe903c1434
Lee <leeganghee4954@naver.com>
2025-02-05 17:36:07 +0900

    chore: main js animation

--- views/js/index.js
+++ views/js/index.js
@@ -1,1 +1,30 @@
-console.log("hi");
+const tagsOnVideo = document.querySelector(".tagsOnVideo");
+const mainVideoDiv = document.querySelector(".mainVideoDiv");
+const videoWrap = document.querySelector(".videoWrap");
+const mainVideo = document.querySelector(".videoWrap > video");
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  if (scroll > 0) {
+    window.scrollY = 100;
+    mainVideoDiv.style.width = "1400px";
+    mainVideoDiv.style.position = "static";
+    mainVideoDiv.style.margin = "250px auto";
+    videoWrap.style.width = "100%";
+    videoWrap.style.height = "380px";
+    videoWrap.style.position = "relative";
+    mainVideo.style.height = "auto";
+    mainVideo.style.transform = "translateY(-50%) scale(0.8)";
+    tagsOnVideo.style.opacity = "0";
+  } else if (scroll <= 0) {
+    mainVideoDiv.style.width = "100%";
+    mainVideoDiv.style.position = "relative";
+    mainVideoDiv.style.margin = "auto";
+    videoWrap.style.width = "auto";
+    videoWrap.style.height = "auto";
+    videoWrap.style.position = "static";
+    mainVideo.style.height = "100%";
+    mainVideo.style.transform = "translateY(0%) scale(1)";
+    tagsOnVideo.style.opacity = "1";
+  }
+});
