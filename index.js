58b52b34d709adeb393ce9ad19f8072e128de371
Lee <leeganghee4954@naver.com>
2025-02-05 15:55:51 +0900

    feat: add main video at main page

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const app = express();
 const bodyparser = require("body-parser");
 const fs = require("fs");
 const http = require("http");
-var mysql=require('mysql2');
+var mysql = require("mysql2");
 //데이터베이스 연결 함수
-function dbcon(){
+function dbcon() {
   db = null;
-  try{
-    file = fs.readFileSync("dbcon.json",'utf8')
-    db = JSON.parse(file)
-  }catch(err){
-    console.log("읽기 에러")
+  try {
+    file = fs.readFileSync("dbcon.json", "utf8");
+    db = JSON.parse(file);
+  } catch (err) {
+    console.log("읽기 에러");
     throw err;
   }
-  const con=mysql.createConnection({
-      host: db.name,
-      user: db.id, password: db.pw,
-      database:'jumakzip',port:'3306'
+  const con = mysql.createConnection({
+    host: db.name,
+    user: db.id,
+    password: db.pw,
+    database: "jumakzip",
+    port: "3306",
   });
   con.connect();
-  console.log('db connect');
+  console.log("db connect");
 }
-const dbconn = dbcon()
+const dbconn = dbcon();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -43,12 +45,39 @@ app.set("layout", "layouts/layout");
 app.set("layout extractScripts", true);
 app.set("layout extractStyle", true);
 
+//video 가져오는 경로
+app.get("/video/:name", function (req, res) {
+  var name = req.params.name;
+  console.log("이미지 요청: " + "./views/video/" + name);
+  res.sendFile(path.join(__dirname, "views", "/video/" + name));
+});
+
+//css 가져오는 경로
+app.get("/css/:name", function (req, res) {
+  var name = req.params.name;
+  console.log("css 요청: " + "./views/css/" + name);
+  res.sendFile(path.join(__dirname, "views", "/css/" + name));
+});
+//js 가져오는 경로
+app.get("/js/:name", function (req, res) {
+  var name = req.params.name;
+  console.log("js 요청: " + "./views/js/" + name);
+  res.sendFile(path.join(__dirname, "views", "/js/" + name));
+});
+
+// 페이지 로드 코드들 ----------------------------------------------------------
+
 //메인페이지 로드
 app.get("/", function (req, res) {
   res.render("index", {
     title: "main",
-    style: "",
+    style: '<link rel="stylesheet" href="/css/index.css">',
   });
+});
+
+//로그인페이지 로드
+app.get("/logIn", function (req, res) {
+  res.render("logIn", { title: "logIn", style: "" });
 });
 
 //예약페이지 로드
@@ -66,17 +95,4 @@ app.get("/image/:name", function (req, res) {
   var name = req.params.name;
   console.log("이미지 요청: " + "./views/image/" + name);
   res.sendFile(path.join(__dirname, "views", "/image/" + name));
-});
-
-//css 가져오는 경로
-app.get("/css/:name", function (req, res) {
-  var name = req.params.name;
-  console.log("css 요청: " + "./views/css/" + name);
-  res.sendFile(path.join(__dirname, "views", "/css/" + name));
-});
-//js 가져오는 경로
-app.get("/js/:name", function (req, res) {
-  var name = req.params.name;
-  console.log("js 요청: " + "./views/js/" + name);
-  res.sendFile(path.join(__dirname, "views", "/js/" + name));
 });
