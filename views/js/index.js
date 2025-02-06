6726eca62d7ccc436eb722e275d355b4fb14bc67
Lee <leeganghee4954@naver.com>
2025-02-06 12:50:18 +0900

    feat: add animation at main page

--- views/js/index.js
+++ views/js/index.js
@@ -2,29 +2,69 @@ const tagsOnVideo = document.querySelector(".tagsOnVideo");
 const mainVideoDiv = document.querySelector(".mainVideoDiv");
 const videoWrap = document.querySelector(".videoWrap");
 const mainVideo = document.querySelector(".videoWrap > video");
+const mainDiv = document.querySelector(".mainDiv");
+const screenHeight = screen.height;
+
+const room1Img = document.querySelector(".room1Img");
+const room1ImgY = room1Img.getBoundingClientRect().y;
+const room1Text = document.querySelector(".room1Text");
+
+const room2Img = document.querySelector(".room2Img");
+const room2ImgY = room2Img.getBoundingClientRect().y;
+const room2Text = document.querySelector(".room2Text");
+
+let lastScrollY = 0;
 
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
-  if (scroll > 0) {
-    window.scrollY = 100;
+
+  if (scroll > lastScrollY && scroll < 100) {
     mainVideoDiv.style.width = "1400px";
     mainVideoDiv.style.position = "static";
-    mainVideoDiv.style.margin = "250px auto";
-    videoWrap.style.width = "100%";
+    mainVideo.style.height = "auto";
+    mainVideo.style.transform = "translateY(-50%)";
+    mainVideoDiv.style.margin = "200px auto 0 auto";
     videoWrap.style.height = "380px";
     videoWrap.style.position = "relative";
-    mainVideo.style.height = "auto";
-    mainVideo.style.transform = "translateY(-50%) scale(0.8)";
     tagsOnVideo.style.opacity = "0";
-  } else if (scroll <= 0) {
+    mainDiv.style.opacity = "1";
+    mainDiv.style.margin = "-600px auto 0 auto";
+  }
+  if (scroll <= lastScrollY && scroll < 100) {
+    window.scrollTo(0, 0);
+    mainVideo.style.height = "100%";
+    mainVideo.style.transform = "translateY(0%)";
+    mainVideoDiv.style.margin = "auto";
     mainVideoDiv.style.width = "100%";
     mainVideoDiv.style.position = "relative";
-    mainVideoDiv.style.margin = "auto";
-    videoWrap.style.width = "auto";
     videoWrap.style.height = "auto";
     videoWrap.style.position = "static";
-    mainVideo.style.height = "100%";
-    mainVideo.style.transform = "translateY(0%) scale(1)";
     tagsOnVideo.style.opacity = "1";
+    mainDiv.style.opacity = "0";
+    mainDiv.style.margin = "0 auto";
   }
+  if (room1ImgY < scroll + screenHeight) {
+    room1Img.style.opacity = "1";
+    room1Img.style.transform = "translateY(0%)";
+    room1Text.style.opacity = "1";
+    room1Text.style.transform = "translateY(0%)";
+  } else {
+    room1Img.style.opacity = "0";
+    room1Img.style.transform = "translateY(30%)";
+    room1Text.style.opacity = "0";
+    room1Text.style.transform = "translateY(30%)";
+  }
+  if (room2ImgY < scroll + screenHeight) {
+    room2Img.style.opacity = "1";
+    room2Img.style.transform = "translateY(0%)";
+    room2Text.style.opacity = "1";
+    room2Text.style.transform = "translateY(0%)";
+  } else {
+    room2Img.style.opacity = "0";
+    room2Img.style.transform = "translateY(30%)";
+    room2Text.style.opacity = "0";
+    room2Text.style.transform = "translateY(30%)";
+  }
+  // 스크롤 올림 내림 확인
+  lastScrollY = scroll;
 });
