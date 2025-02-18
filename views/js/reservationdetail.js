481837d74c28749ed7e00041359e8e764aa8e210
Lee <leeganghee4954@naver.com>
2025-02-18 11:17:23 +0900

    feat: 헤더 레이아웃 에러와 reservation session중복 호출 에러 수정

--- views/js/reservationdetail.js
+++ views/js/reservationdetail.js
@@ -1,15 +1,12 @@
-jQuery(document).ready(function(){
-    number=0
-    $('.span1').click(function(){
-        number+=1
+jQuery(document).ready(function () {
+  number = 0;
+  $(".span1").click(function () {
+    number += 1;
 
-        if(number%2==1){
-            $(this).addClass('active')
-        }
-        else{
-            $(this).removeClass('active')
-        }
-
-    })
-    
+    if (number % 2 == 1) {
+      $(this).addClass("active");
+    } else {
+      $(this).removeClass("active");
+    }
+  });
 });
